1962e48d94c380e71d45d9d25689703c1d202317
minomi <5minhominho@gmail.com>
2017-11-20 18:10:23 +0900

    CoreData Tutorial

---
+++ HitList/HitList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by 오민호 on 2017. 11. 20..
+//  Copyright © 2017년 오민호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
